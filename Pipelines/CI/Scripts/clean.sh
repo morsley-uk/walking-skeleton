c959f5d457f1a062c80b4012f1832876daba3c91
John Morsley <johnmorsley@gmail.com>
2020-01-30 21:11:16 +0000

    .

--- Pipelines/CI/Scripts/clean.sh
+++ Pipelines/CI/Scripts/clean.sh
@@ -6,7 +6,9 @@ source $common/header.sh
 
 header 'CLEAN STARTED'
 
-dotnet clean --verbosity normal --nologo
+echo parent_path
+
+#dotnet clean --verbosity normal --nologo
 
 #find . -iname "bin" | xargs rm -rf
 #find . -iname "obj" | xargs rm -rf
