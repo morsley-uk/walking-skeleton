8dc2c052c09b36b92cca47bfd244cf2e39517114
John Morsley <johnmorsley@gmail.com>
2020-01-31 16:44:02 +0000

    .

--- Pipelines/CI/Scripts/build-and-publish.sh
+++ Pipelines/CI/Scripts/build-and-publish.sh
@@ -4,13 +4,15 @@ parent_path=$(cd "$(dirname "${BASH_SOURCE[0]}")";pwd -P)
 common="$parent_path"/../../Scripts
 source $common/header.sh
 
+ls -la
 cd source-master
+ls -la
 
 ###############################################################################
 
 header 'CLEAN STARTED'
 
-dotnet clean --verbosity normal --nologo
+#dotnet clean --verbosity normal --nologo
 
 header 'CLEAN COMPLETED'
 
@@ -18,7 +20,7 @@ header 'CLEAN COMPLETED'
 
 header 'RESTORE STARTED'
 
-dotnet restore --verbosity normal
+#dotnet restore --verbosity normal
 
 header 'RESTORE COMPLETED'
 
@@ -26,7 +28,7 @@ header 'RESTORE COMPLETED'
 
 header 'BUILD STARTED'
 
-dotnet build --verbosity normal --configuration Release --no-restore
+#dotnet build --verbosity normal --configuration Release --no-restore
 
 header 'BUILD COMPLETED'
 
@@ -34,7 +36,7 @@ header 'BUILD COMPLETED'
 
 header 'PUBLISH STARTED'
 
-dotnet publish --verbosity normal --configuration Release --no-build --no-restore --output Published --nologo
+#dotnet publish --verbosity normal --configuration Release --no-build --no-restore --output Published --nologo
 
 header 'PUBLISH COMPLETED'
 
