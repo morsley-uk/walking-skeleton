33c13b3e1c40e7554fc320f041ca62416d52ba99
John Morsley <johnmorsley@gmail.com>
2020-02-02 21:35:14 +0000

    .

--- Pipelines/CI/Scripts/clean-restore-build-publish.sh
+++ Pipelines/CI/Scripts/clean-restore-build-publish.sh
@@ -18,7 +18,7 @@ header 'CLEAN COMPLETED'
 
 header 'RESTORE STARTED'
 
-#dotnet restore --verbosity normal
+dotnet restore --verbosity normal
 
 header 'RESTORE COMPLETED'
 
@@ -26,7 +26,7 @@ header 'RESTORE COMPLETED'
 
 header 'BUILD STARTED'
 
-#dotnet build --verbosity normal --configuration Release --no-restore
+dotnet build --verbosity normal --configuration Release --no-restore
 
 header 'BUILD COMPLETED'
 
@@ -34,7 +34,7 @@ header 'BUILD COMPLETED'
 
 header 'PUBLISH STARTED'
 
-#dotnet publish --verbosity normal --configuration Release --no-build --no-restore --output Published --nologo
+dotnet publish --verbosity normal --configuration Release --no-build --no-restore --output Published --nologo
 
 header 'PUBLISH COMPLETED'
 
