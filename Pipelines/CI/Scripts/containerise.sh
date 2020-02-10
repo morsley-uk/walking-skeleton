d13dd23af092977786c213e9bd594f4b47789d41
John Morsley <johnmorsley@gmail.com>
2020-02-10 00:27:51 +0000

    .

--- Pipelines/CI/Scripts/containerise.sh
+++ Pipelines/CI/Scripts/containerise.sh
@@ -2,11 +2,13 @@
 
 set -x
 
+ls -la
+
 PARENT_PATH=$(cd "$(dirname "${BASH_SOURCE[0]}")";pwd -P)
 ROOT="$PARENT_PATH"/../../..
 CI="$ROOT"/Pipelines/CI
-SCRIPTS="$ROOT"/Pipelines/Scripts
-source $SCRIPTS/header.sh
+#SCRIPTS="$ROOT"/Pipelines/Scripts
+#source $SCRIPTS/header.sh
 
 ls -la
 
@@ -14,7 +16,7 @@ cd tested
 
 ###############################################################################
 
-header 'CREATE DOCKER IMAGE'
+#header 'CREATE DOCKER IMAGE'
 
 cd $CI
 
@@ -22,13 +24,13 @@ ls -la
 
 docker build --tag morsley-uk-walking-skeleton-api .
 
-header 'CREATE DOCKER IMAGE'
+#header 'CREATE DOCKER IMAGE'
 
 ###############################################################################
 
-header 'UPLOAD DOCKER IMAGE'
+#header 'UPLOAD DOCKER IMAGE'
 
 
-header 'UPLOAD DOCKER IMAGE'
+#header 'UPLOAD DOCKER IMAGE'
 
 ###############################################################################
