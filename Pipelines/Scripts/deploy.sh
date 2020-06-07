dd30d9829e267b489466193b6aa3d11ed2f696fc
John Morsley <johnmorsley.gmail.com>
2020-06-07 03:24:22 +0100

    .

--- Pipelines/Scripts/deploy.sh
+++ Pipelines/Scripts/deploy.sh
@@ -4,17 +4,17 @@ set -x
 
 PARENT_PATH=$(cd "$(dirname "${BASH_SOURCE[0]}")";pwd -P)
 
-ROOT=${PARENT_PATH}/..
+#ROOT=${PARENT_PATH}/..
 #SCRIPTS=${PARENT_PATH}/Pipelines/Scripts
 #source $SCRIPTS/header.sh
 
-echo "PARENT_PATH:" ${PARENT_PATH}
+#echo "PARENT_PATH:" ${PARENT_PATH}
 #echo "ROOT:" ${ROOT}
-echo "SCRIPTS:" ${SCRIPTS}
+#echo "SCRIPTS:" ${SCRIPTS}
 
 ls -la
 
-cd ${ROOT}
+cd deploy
 
 ls -la
 
