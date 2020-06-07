2aad46aa3958b2d105667d9fcd28de466723a9cf
John Morsley <johnmorsley.gmail.com>
2020-06-07 04:22:08 +0100

    .

--- Pipelines/Scripts/deploy.sh
+++ Pipelines/Scripts/deploy.sh
@@ -6,6 +6,18 @@ SCRIPTS=${WD}/deploy/Pipelines/Scripts
 echo "SCRIPTS:" ${SCRIPTS}
 source ${SCRIPTS}/header.sh
 
+ARTIFACTS=${WD}/deploy/Artifacts
+echo "ARTIFACTS:" ${ARTIFACTS}
+
+K8S=${WD}/deploy/k8s
+echo "K8S:" ${K8S}
+
 header 'DEPLOY'
+
+echo "Artifacts:"
+cd ${ARTIFACTS} && ll -la
+
+echo "Kubernetes:"
+cd ${K8S} && ll -la
 
 kubectl version --client
