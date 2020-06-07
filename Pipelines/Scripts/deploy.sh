729778fc23c8301c97ff02b06aed46dcfd49e29e
John Morsley <johnmorsley.gmail.com>
2020-06-07 04:00:04 +0100

    .

--- Pipelines/Scripts/deploy.sh
+++ Pipelines/Scripts/deploy.sh
@@ -1,29 +1,11 @@
 #!/bin/bash
 
-#ls -la
-
-#set -x
-
-#PARENT_PATH=$(cd "$(dirname "${BASH_SOURCE[0]}")";pwd -P)
-
 WD=$(pwd)
 echo "WD:" ${WD}
-#cd ${WD}
-#ls -la
-
 SCRIPTS=${WD}/deploy/Pipelines/Scripts
-#echo "ROOT:" ${ROOT}
-#cd ${ROOT}
-#ls -la
-
-#SCRIPTS=${ROOT}/Pipelines/Scripts
 echo "SCRIPTS:" ${SCRIPTS}
-cd ${SCRIPTS}
-ls -la
 source ${SCRIPTS}/header.sh
 
-#echo "PARENT_PATH:" ${PARENT_PATH}
-#cd ${PARENT_PATH}
-#ls -la
+header 'DEPLOY'
 
 kubectl version --client
